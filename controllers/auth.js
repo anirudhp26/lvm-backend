9f81ca3e4deb12ead1e759ac8c10abcd299e0818
Anirudh Patel <anirudh26p@gmail.com>
2023-08-13 21:07:07 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,7 +11,7 @@ export const login = async (req, res) => {
         User.find({ googleId: googleId }, (err, responce) => {
             if (responce.length === 0) {
                 const token = jwt.sign({ id: googleId }, process.env.JWT_SECRET);
-                res.status(201).json({ message: "You'll be redirected to edit profile page where you can add a username to your account", token});              
+                res.status(201).json({ message: "You'll be redirected to edit profile page where you can add a username to your account", token });
             } else {
                 const token = jwt.sign({ id: responce[0]._id }, process.env.JWT_SECRET);
                 res.status(200).json({ user: responce[0], loginStatus: true, token });
@@ -44,7 +44,7 @@ export const login = async (req, res) => {
     }
 };
 
-export const checkUser = async (req,res) => {
+export const checkUser = async (req, res) => {
     const username = req.body.username;
     const responce = await User.find({ username: username });
     if (responce.length > 0) {
@@ -54,18 +54,18 @@ export const checkUser = async (req,res) => {
     }
 }
 
-export const getUsers = async (req,res) => {
+export const getUsers = async (req, res) => {
     const search = req.body.keyword || "";
     if (search === "") {
         res.send([]);
     }
-    else{
-        User.find({username: {$regex: search, $options: "i"}}, (err,responce) => {
+    else {
+        User.find({ username: { $regex: search, $options: "i" } }, (err, responce) => {
             if (err) {
                 res.send(err);
             }
-            else{
-                responce.forEach(function(v){ v.password = undefined });
+            else {
+                responce.forEach(function (v) { v.password = undefined });
                 res.send(responce);
             }
         })
@@ -76,6 +76,15 @@ export const updateUser = async (req, res) => {
     const googleUserUpdate = req.body.googleUserUpdate;
     if (googleUserUpdate) {
         let suser = req.body.user;
+        if (suser.username === undefined || suser.username === "") {
+            suser.username = suser.name.trim() + suser.sub.substr(suser.sub.length - 3);
+        }
+        const resp = await User.find({ username: suser.username });
+        if (resp.length > 0) {
+            res.status(201).json({ username_available: false });
+        } else {
+            res.status(200).json({ username_available: true });
+        }
         suser.password = suser.sub;
         suser.googleId = suser.sub;
         const user = new User(suser);
@@ -84,11 +93,11 @@ export const updateUser = async (req, res) => {
             const token = jwt.sign({ id: responce._id }, process.env.JWT_SECRET);
             res.status(200).json({ loginStatus: true, updatedUser: responce, token });
         } else {
-            res.status(201).json({ loginStatus: false, message: "Error occured, please try again after sometime"})
+            res.status(201).json({ loginStatus: false, message: "Error occured, please try again after sometime" })
         }
     } else {
         const suser = req.body.user;
-        const user = await User.findOne({_id: suser._id});
+        const user = await User.findOne({ _id: suser._id });
         Object.assign(user, suser);
         const responce = user.save();
         if (responce) {
@@ -99,21 +108,21 @@ export const updateUser = async (req, res) => {
     }
 }
 
-export const signup = async (req,res) => {
+export const signup = async (req, res) => {
     try {
         const username = req.body.username;
         const name = req.body.name;
         const bio = req.body.bio;
         const regpassword = req.body.password;
-        const newPassword = await bcrypt.hash(regpassword,salt);
-        User.find({username: username}, async (err,resp) => {
+        const newPassword = await bcrypt.hash(regpassword, salt);
+        User.find({ username: username }, async (err, resp) => {
             if (err) {
                 console.log(err);
                 res.status(200).send({ message: "Some error has occured, please try again later.!", loginStatus: false });
             } else if (resp.length > 0) {
                 res.status(200).send({ message: "Username already Exists", loginStatus: false });
             } else {
-                const user = new User({username, password: newPassword, name: name, bio: bio});
+                const user = new User({ username, password: newPassword, name: name, bio: bio });
                 const responce = await user.save();
                 const token = jwt.sign({ id: responce._id }, process.env.JWT_SECRET);
                 responce.password = undefined;
