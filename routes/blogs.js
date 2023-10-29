3c56afd83cf1b067cff9c79f9dee398b43ae38a0
Anirudh Patel <anirudh26p@gmail.com>
2023-10-30 02:07:50 +0530

    Update blogs.js

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import { addComment, deleteBlog, getblogbyid, getblogsbykeywords, getblogsbyuser, recommendation, updateBlog } from '../controllers/blogs.js';
+import { addComment, deleteBlog, getblogbyid, getblogsbykeywords, getblogsbyuser, personalized, trending, updateBlog } from '../controllers/blogs.js';
 
 const router = express.Router();
 
 router.post('/getBlogbyID', getblogbyid);
 router.post('/getBlogbyUser', getblogsbyuser);
 router.post('/getBlogbyKeywords', getblogsbykeywords);
-router.post('/getRecommendations', recommendation);
+router.post('/getTrending', trending);
+router.post('/getPersonalized', personalized);
 router.post('/updateBlog', updateBlog);
 router.post('/deleteBlog', deleteBlog);
 router.post('/addcomment', addComment);
